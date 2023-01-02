e390fad78c3b2d1db7c96326a2962062697a91fa
Alessio Di Nardo <alessio.dinardo91@gmail.com>
2023-01-02 16:17:54 +0100

    first commit

---
+++ MVVM_DataBinding_Singleton/MVVM_DataBinding_Singleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_DataBinding_Singleton
+//
+//  Created by Alessio Di Nardo on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
